78752cb995ba846af738cb526478d086aebe3550
gwansik <rhkstlr00@gmail.com>
2021-11-07 21:00:46 +0900

    javascript array study

--- study.js
+++ study.js
@@ -35,18 +35,22 @@ mycat.showName();
 // Array함수의 map - 요소에 맞는 똑같은 갯수의 배열을 가져온다. 즉, 변수에 선언된 배열을 그대로 가져온다.
 const array_num = [0, 1, 2, 3, 4, 5]; //변수 선언
 
-const array_nums = array_num.map((array_item) => { //array_num이라는 변수에 map을 씌운다. (map은 변수에 선언된 배열들을 전부 불러오는 것)
+const array_number = array_num.map((array_item) => { //array_num이라는 변수에 map을 씌운다. (map은 변수에 선언된 배열들을 전부 불러오는 것)
     console.log(array_item); // array_item의 배열들이 잘 들어오는지 확인
     return array_item + 1;
 })
-console.log(array_nums);
+console.log(array_number);
 
 
 // Array함수의 fillter- 어떤 조건을 만족하는 항목들만 골라서 새로운 배열을 만들어준다. (조건에 맞는 요소들만 가져온다.)
 const new_array = [0, 1, 2, 3, 4, 5]; //변수 선언
 
-const new_arrays = new_array.filter((array_item) => {
+const new_array_number = new_array.filter((array_item) => {
     return array_item > 3;
 })
+console.log(new_array_number);
 
-console.log(new_arrays)
+// Array함수의 concat - 서로 다른 배열을 합쳐준다. 대신 중복제거는 하지 않는다.
+const merge = array_num.concat(new_array_number);
+console.log(merge);
+
