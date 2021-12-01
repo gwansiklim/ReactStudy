d194789cf661f3f4d49e8a703eacc36e3b975f58
gwansik <rhkstlr00@gmail.com>
2021-12-01 14:54:36 +0900

    styled-components 사용방법

--- bucket_list/src/App.js
+++ bucket_list/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import BucketList from './BucketList';
 import './style.css';
+import styled from 'styled-components';
 
 
 // 클래스형 컴포넌트
@@ -17,19 +18,24 @@ class App extends React.Component {
         console.log(this.state.list);
         return (
             <div className="App">
-                <div className="container">
+                <MyStyled>
+                    <p>
+                        im here!!!!!
+                    </p>
+
+                </MyStyled>
+                {/* <div className="container">
                     <h1>나의 버킷리스트</h1>
                     <hr className="line" />
                     <BucketList list={this.state.list} />
-                </div>
-
+                </div> */}
             </div>
         )
     }
 }
 
 
-// 함수형 컴포넌트
+//함수형 컴포넌트
 // function App() {
 //     return (
 //         <div className="App">
@@ -37,5 +43,21 @@ class App extends React.Component {
 //         </div>
 //     );
 // }
+
+
+const MyStyled = styled.div`
+    width: 50vw;
+    height: 150px;
+    background-color: ${(props) => (props.bg_color ? "red" : "purple")};
+    p {
+        color: blue; 
+        &:hover {
+            color: red;
+        }
+    }
+    &:hover {
+        background-color: yellow;
+    }
+`;
 
 export default App;
