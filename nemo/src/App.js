a803340c6ecef2c89182256b4614ef975a050a10
gwansik <rhkstlr00@gmail.com>
2021-12-02 20:39:00 +0900

    class형으로 state관리(1)

---
+++ nemo/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 3,
+        };
+    }
+
+    componentDidMount() { }
+
+    addNemo = () => {
+        this.setState({ count: this.state.count + 1 });
+    }
+    removeNemo = () => {
+        if (this.state.count > 0) {
+            this.setState({ count: this.state.count - 1 })
+        }
+
+    }
+
+    render() {
+        const nemo_count = Array.from({ length: this.state.count }, (v, i) => i);
+
+        console.log(this.state);
+        return (
+            <div className="App">
+                {nemo_count.map((n, i) => {
+                    return (
+                        <div
+                            key={i}
+                            style={{
+                                width: "150px",
+                                height: "150px",
+                                backgroundColor: "#ddd",
+                                margin: "10px",
+                            }}>
+                            nemo
+                        </div>
+                    )
+                }
+                )}
+
+                <div>
+                    <button onClick={this.addNemo}>추가하기</button>
+                    <button onClick={this.removeNemo}>지우기</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
