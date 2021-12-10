9eeaba11f5d5056aa0350be64ef3c3155e57fe85
gwansik <rhkstlr00@gmail.com>
2021-12-10 14:35:06 +0900

    디테일에서 삭제기능 넣기

--- bucket_list/src/Detail.js
+++ bucket_list/src/Detail.js
@@ -1,17 +1,25 @@
 //디테일 페이지
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteBucket } from './redux/modules/bucaket';
+import { useHistory } from 'react-router-dom';
 
 const Detail = (props) => {
+    const dispatch = useDispatch();
+    const history = useHistory();
     const params = useParams();
     const bucket_index = params.index;
     const bucket_list = useSelector((state) => state.bucket.list); //리덕스에서 리스트 가져오기
 
-    console.log(bucket_list);
-    console.log(bucket_index);
     return (
-        <h1>{bucket_list[bucket_index]}</h1>
+        <div>
+            <h1>{bucket_list[bucket_index]}</h1>
+            <button onClick={() => {
+                dispatch(deleteBucket(bucket_index));
+                history.goBack();
+            }}>삭제하기</button>
+        </div>
     )
 }
 
--- bucket_list/src/redux/modules/bucaket.js
+++ bucket_list/src/redux/modules/bucaket.js
@@ -1,20 +1,26 @@
 // bucket.js
 
-// Actions
+// Actions Type 지정해주기!
 const CREATE = 'bucket/CREATE';
+const DELETE = 'bucket/DELETE';
 
+//리덕스에 정보 저장
 const initilaState = {
-    list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
+    list: ["영화관 가기", "매일 책읽기", "수영 배우기", "코딩하기"],
 }
 
-// Action Creators
+// Action Creators - 정보 생성
 export function createBucket(bucket) {
     console.log("액션 생성!");
     return { type: CREATE, bucket: bucket };
 }
+//Action Delete -삭제하기
+export function deleteBucket(bucket_index) {
+    console.log("지울 버킷 인덱스", bucket_index);
+    return { type: DELETE, bucket_index };
+}
 
-
-// Reducer
+// Reducer - state에 정보를 바꿔 주는 역할
 export default function reducer(state = initilaState, action = {}) {
     switch (action.type) {
         case "bucket/CREATE": {
@@ -22,6 +28,15 @@ export default function reducer(state = initilaState, action = {}) {
             const new_bucket_list = [...state.list, action.bucket];
             return { list: new_bucket_list };
         }
+
+        case "bucket/DELETE": {
+            console.log(state, action);
+            const new_bucket_list = state.list.filter((l, idx) => {
+                return parseInt(action.bucket_index) !== idx;
+            })
+            return { list: new_bucket_list };
+        }
+
         default: return state;
     }
 }
